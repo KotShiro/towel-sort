a633b34fa918a5f2c81ab70226ca8e7330430113
Никита А. Ковалевич <C:\Users\kovalevich\AppData\Roaming\The Bat!>
2020-10-13 16:09:17 +0300

    refactor: changed the index.js file to execute This task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return []; 
+  let outMatrix = [];
+  matrix.forEach((element, ind) => {
+  ind % 2 !== 0 ?element = element.reverse() :  element ;
+    element.forEach((element) => {
+      outMatrix.push(element);
+    })
+  })
+  return outMatrix;
 }
